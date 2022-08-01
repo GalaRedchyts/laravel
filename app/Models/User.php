609e3cda34872b88af239e0eac2d2492188d2ae8
galynkared@gmail.com <galynkared@gmail.com>
2022-08-01 22:37:07 +0200

    IsAdmin helper, Admin middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\Enums\Roles;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -26,7 +27,6 @@ class User extends Authenticatable
         'birthdate',
         'password',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
