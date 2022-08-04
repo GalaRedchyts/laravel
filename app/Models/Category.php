7bf2da971f75059f6ce2b2315276e1cc51e0c8e7
galynkared@gmail.com <galynkared@gmail.com>
2022-08-04 23:54:13 +0200

    Category, Product, Image models

---
+++ app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['id', 'name', 'description'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    //полиморфная связь к классу image
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
